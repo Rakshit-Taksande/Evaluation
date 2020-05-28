8b10395fc8c8d22025643167a3fde13bc2baed72
Rakshit taksande <Rakshittaksande@gmail.com>
2020-05-28 19:41:06 +0530

    task1

---
+++ HackerNews.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hacker News</title>
+  <link rel="stylesheet" type="text/css" href="style.php"/>
+</head>
+
+<body>
+  <section>
+    <header>
+      <div id="logo">Y</div>
+      <p><b>Hacker News</b></p>
+      <p> new | past | comments | ask | show | jobs | submit</p>
+      <p class="right">login</p>
+    </header>
+    
+
+    <p id="more">More</p>
+
+    <footer>
+        <nav class="small">
+        Guidelines <span class="gray">|</span> FAQ <span class="gray">|</span> Support <span class="gray">|</span> API <span class="gray">|</span> Security <span class="gray">|</span> Lists <span class="gray">|</span> Bookmarklet <span class="gray">|</span> Legal <span class="gray">|</span> Apply to YC <span class="gray">|</span> Contact
+      </nav>
+  
+      <label class="gray" for="search">Search:</label>
+      <input type="text" id="search">
+    </footer>
+
+  </section>
+</body>
+</html>
---
+++ style.php
@@ -0,0 +1,89 @@
+<?php
+    header('content-type: text/css; charset:UTF-8');
+?>
+* {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      font-family: Verdana, Geneva, Tahoma, sans-serif;
+    }
+
+    p {
+      font-size: .85rem;
+    }
+
+    header {
+      background: rgb(255, 102, 2);
+      padding: .15rem;
+    }
+
+    section {
+      background: rgb(246,246,239);
+      margin: 5px auto 0;
+      width: 80%;
+    }
+
+    section ol {
+      margin: 3px 0 0 1rem;
+    }
+
+    section li {
+      margin: .5rem 0;
+    }
+
+    #more {
+      margin: 1.1rem 0 0.9rem;
+    }
+
+    .right {
+      float: right;
+      margin-right: .25rem;
+    }
+
+    footer {
+      border-top: 2px solid rgb(255, 102, 2);
+      text-align: center;
+    }
+
+    .small {
+      font-size: .8rem;
+    }
+    .smaller {
+      font-size: .7rem;
+    }
+    .gray {
+      color: rgb(137,137,137);
+    }
+
+    #logo {
+      border: 1px solid white;
+      height: 24px;
+      width: 24px;
+      text-align: center;
+      font-size: 1rem;
+      padding-top: 2.5px;
+      margin-right: .3rem;
+      color: white;
+      font-weight: bold;
+    }
+
+    label {
+      display: inline-block;
+    }
+
+    footer {
+      padding: 1.25rem;
+    }
+
+    footer nav {
+      margin: 1.25rem 0;
+    }
+
+    .triangle {
+      margin-right: 1.5px;
+      color: rgb(153,153,153);
+    }
+
+    .title-container p {
+      display: inline-block;
+    }
